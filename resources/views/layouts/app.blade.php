c2762f79cb31a53f4c5a92bb9a81de2b0bd0554e
Rushan Tamrakar <rushan2072@gmail.com>
2025-03-11 22:52:03 +0545

    template and blade layout implemented

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getlocale())}}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title') | {{ config('app.name') }}</title>
-</head>
-<body>
-    <header>Header</header>
-        @yield('content')
-    <footer>Footer</footer>
-</body>
-</html>
+@extends('layouts.clean')
+@section('childContent')
+    @include('layouts.partials.header')
+    @yield('content')
+    <footer></footer>
+@endsection
+    
+
